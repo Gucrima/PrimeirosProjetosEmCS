be54411fe31930a916ab80a9b1830256d68e85c2
Gustavo <gusssoares09@gmail.com>
2025-04-11 00:01:40 -0300

    testando algumas coisas

---
+++ AtividadesEmC#/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AtividadeEmCS {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Pessoal Curioso....");
+        }
+    }
+}
