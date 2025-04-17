a2910d5783b614acb077ec0d6e1cd44142b459cd
Gustavo <gusssoares09@gmail.com>
2025-04-17 04:00:44 -0300

    add contador no cmd com enter

---
+++ contadorTeste/contadorTeste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BackContador {
+
+    class Program {
+        static int contador = 0;
+
+        static void Main(string[] args) {
+            Console.WriteLine("Bem-vindo ao contador!");
+            Console.WriteLine($"Contador atual: {contador}");
+
+            Console.WriteLine("Pressione Enter para incrementar o contador...");
+            while (true) {
+                Console.ReadLine();
+                contador++;
+                Console.WriteLine($"Contador atualizado: {contador}");
+            }
+        }
+    }
+}
